151c94599afb84947fbab60fd7c1f4a411ce61cc
Evandro2270 <evandro535@gmail.com>
2024-07-12 14:59:10 -0300

    Adicionar arquivos de projeto.

---
+++ cond02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int N;
+
+                N = int.Parse(Console.ReadLine());
+
+                if (N % 2 == 0)
+                {
+                    Console.WriteLine("PAR");
+                }
+                else
+                {
+                    Console.WriteLine("IMPAR");
+                }
+            }
+        }
+    }
+}
